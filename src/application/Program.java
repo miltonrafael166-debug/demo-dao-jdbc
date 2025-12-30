ed6a639eca88f8831a62a4ac41123d62d4e764a1
MiltonRafael <miltonrafael166@gmail.com>
2025-12-30 10:11:46 -0300

    FindAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,12 @@ public class Program {
 			System.out.println(obj);
 		} 
 
+		System.out.println();
+		System.out.println("===TEST 3: SELLER FINDBYALL===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
+		} 
 		
 	}
 
